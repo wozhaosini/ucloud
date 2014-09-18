181385e3fc2c5efc73f9c189f3b356a5e5ebcddc
luojie <luojie715@foxmail.com>
2014-09-18 14:05:46 +0800

    格式化

--- ucloud-all/ucloud-usermanage/src/main/java/org/dlut/ucloud/usermanage/App.java
+++ ucloud-all/ucloud-usermanage/src/main/java/org/dlut/ucloud/usermanage/App.java
@@ -2,12 +2,9 @@ package org.dlut.ucloud.usermanage;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
--- ucloud-all/ucloud-vmmanage/src/main/java/org/dlut/ucloud/vmmanage/App.java
+++ ucloud-all/ucloud-vmmanage/src/main/java/org/dlut/ucloud/vmmanage/App.java
@@ -2,13 +2,10 @@ package org.dlut.ucloud.vmmanage;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println("haha");
     }
 }
