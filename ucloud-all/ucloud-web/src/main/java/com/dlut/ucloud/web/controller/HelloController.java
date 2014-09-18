29fb276d24942fb68a8088959758278854025646
luojie <luojie715@foxmail.com>
2014-09-19 00:17:23 +0800

    新建工程

--- ucloud-all/ucloud-web/src/main/java/com/dlut/ucloud/web/controller/HelloController.java
+++ ucloud-all/ucloud-web/src/main/java/com/dlut/ucloud/web/controller/HelloController.java
@@ -7,24 +7,33 @@
  */
 package com.dlut.ucloud.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 类HelloController.java的实现描述
  * 
  * @author luojie.lj 2014年9月18日 下午8:36:38
  */
-public class HelloController implements Controller {
+@RequestMapping("/hello.htm")
+@Controller
+public class HelloController {
 
-    public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("message", "xixi");
-        mv.setViewName("hello");
-        return mv;
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public String hello(Model model) {
+		model.addAttribute("message", "xixi");
+		return "hello";
+	}
+	
+//	public ModelAndView handleRequest(HttpServletRequest arg0,
+//			HttpServletResponse arg1) throws Exception {
+//		ModelAndView mv = new ModelAndView();
+//		mv.addObject("message", "xixi");
+//		mv.setViewName("hello");
+//
+//		return mv;
+//	}
 
 }
